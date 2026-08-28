24965c475f99da92fd4e84325a05f2866aab54a7
Munashe270891 <munashesoka7@gmail.com>
2026-08-28 16:18:18 +0200

    Initialize React application with root rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element. Ensure index.html has a <div id="root"></div> wrapper.')
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
